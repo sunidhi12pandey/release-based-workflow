a1379cc80b2aea7b7514d987e2eee65bd88516e7
sunidhi pandey <157216398+sunidhi12pandey@users.noreply.github.com>
2025-11-04 23:59:23 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-eevar sprites = {
+var sprites = {
   ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
   missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
   enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
